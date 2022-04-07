2b895ecc73e265bbe4c7c703ecf94c615e99b3e8
Guru Darji <gurudarji@ymail.com>
2022-04-07 11:16:41 -0400

    cookie added

--- models/index.js
+++ models/index.js
@@ -1,15 +1,16 @@
 const User = require('./User');
 const Listing = require('./Listing');
+const { append } = require('express/lib/response');
+const { application } = require('express');
 
-User.hasMany(Listing{
+User.hasMany(Listing, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Listing.belongsTo(User{
+Listing.belongsTo(User, {
   foreignKey: 'user_id'
 });
-
 
 
 module.exports = { User, Listing};
