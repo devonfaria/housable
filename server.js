16a101213bf48fad875530d4fbd646c9ab5763d6
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2022-04-07 12:08:39 -0400

    updated the form in listings and added a name input for the signup. Currently the log in is not pulling in the post request when we try to log in

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
+const exphbs = require('express-handlebars');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -23,8 +26,14 @@ const sess = {
 
 app.use(session(sess));
 
+const hbs = exphbs.create({  });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
